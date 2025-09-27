02dc0382e68adf48c829fc45d530658137e76f56
haymanmk <haymanmk@gmail.com>
2025-09-27 19:30:52 +0800

    update

---
+++ machine_level_represent_prog/src/lib_1.c
@@ -0,0 +1,9 @@
+#include "lib_1.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
---
+++ machine_level_represent_prog/src/lib_1.h
@@ -0,0 +1,7 @@
+#ifndef __LIB_1_H__
+#define __LIB_1_H__
+
+int add(int a, int b);
+int sub(int a, int b);
+
+#endif /* __LIB_1_H__ */
