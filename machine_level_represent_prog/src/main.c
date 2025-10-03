b338059073e40aaa89417e799a80be07153280b2
haymanmk <haymanmk@gmail.com>
2025-10-03 10:19:37 +0800

    update

--- machine_level_represent_prog/src/main.c
+++ machine_level_represent_prog/src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lib_1.h"
 
 int main() {
     int a = 5;
     int b = 10;
-    int sum = a + b;
+    int sum = add(a, b);
     printf("Sum: %d\n", sum);
     return 0;
 }
